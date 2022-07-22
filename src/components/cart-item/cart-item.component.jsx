c1de31ff51de9c61987b66c93e5ae64785ef62d2
Christian Dean <christiandeandemesa@gmail.com>
2022-07-22 16:31:57 -0700

    Cart count functionality and styled cart items

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import './cart-item.styles.scss';
+
 // Each item in the cart component.
 function CartItem(props) {
 	const {cartItem} = props;
-	const {name, quantity} = cartItem;
+	const {name, imageUrl, price, quantity} = cartItem;
 
 	return (
-		<div>
-			<h2>{name}</h2>
-			<span>{quantity}</span>
+		<div className='cart-item-container'>
+			<img src={imageUrl} alt={name} />
+			<div className='item-details'>
+				<span className='name'>{name}</span>
+				<span className='price'>
+					{quantity} x ${price}
+				</span>
+			</div>
 		</div>
 	);
 }
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import {createContext, useState} from 'react';
+import {createContext, useState, useEffect} from 'react';
 
 // This function adds a product to all the cart items.
 export const addCartItem = (cartItems, productToAdd) => {
@@ -18,17 +18,24 @@ export const CartContext = createContext({
 	isCartOpen: false,
 	setIsCartOpen: () => {},
 	cartItems: [],
-	addItemToCart: () => {}
+	addItemToCart: () => {},
+	cartCount: 0
 });
 
-// This component checks if the cart dropdown menu is open (i.e. true) or closed (i.e. false), and if there are items in the cart.
+// This component checks if the cart dropdown menu is open (i.e. true) or closed (i.e. false), if there are items in the cart, and how many items are in the cart.
 export const CartProvider = props => {
 	const [isCartOpen, setIsCartOpen] = useState(false);
 	const [cartItems, setCartItems] = useState([]);
+	const [cartCount, setCartCount] = useState(0);
+
+	useEffect(() => {
+		const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity, 0);
+		setCartCount(newCartCount);
+	}, [cartItems]);
 
 	const addItemToCart = productToAdd => setCartItems(addCartItem(cartItems, productToAdd));
 
-	const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems};
+	const value = {isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartCount};
 
 	return <CartContext.Provider value={value}>{props.children}</CartContext.Provider>;
 };
