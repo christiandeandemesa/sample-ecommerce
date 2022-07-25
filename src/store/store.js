e91bf94fa1c7011b06e355663a5b66d8fa52774e
Christian Dean <christiandeandemesa@gmail.com>
2022-07-24 20:25:39 -0700

    Persist cart items between reloads

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,24 @@
 // Where each reducer's state lives and where we receive dispatched actions.
 
 import {compose, createStore, applyMiddleware} from 'redux';
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import {rootReducer} from './root-reducer';
+
+const persistConfig = {
+	key: 'root',
+	storage: storage,
+	blacklist: ['user'] // Prevents the the logged in/out user from persisting between reloads.
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [logger]; // This executes before a dispatched action reaches the reducers.
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers); // The second argument is for additional default states.
+export const store = createStore(persistedReducer, undefined, composedEnhancers); // The second argument is for additional default states.
+
+export const persistor = persistStore(store);
